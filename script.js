b69fafcb1f16c95b695d3783c4ee6ea411ed95ae
Mindes <97881522+mindes-a@users.noreply.github.com>
2022-08-24 14:49:31 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("day").innerHTML = new Date();
+let btn = document.getElementById("btn");
+btn.addEventListener("click",function(){
+	location.href ="https://www.amazon.co.jp/%E3%82%A4%E3%83%BC%E3%82%BF%E3%83%83%E3%82%AF-%E3%82%A4%E3%83%BC%E3%82%BF%E3%83%83%E3%82%AF%E6%8A%97%E8%8F%8C%E5%8C%96%E3%82%B9%E3%83%97%E3%83%AC%E3%83%BC%CE%B1-%E3%82%A2%E3%83%AB%E3%82%B3%E3%83%BC%E3%83%AB%E3%82%BF%E3%82%A4%E3%83%97/dp/B099RD3YTK/ref=asc_df_B099RD3YTK/?tag=jpgo-22&linkCode=df0&hvadid=529724945015&hvpos=&hvnetw=g&hvrand=12685644889788924902&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1009233&hvtargid=pla-1410181181519&psc=1&th=1&psc=1"
+});
